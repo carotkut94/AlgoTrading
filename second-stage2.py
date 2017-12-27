c3f6cc079356cfcfa56a82cf4cc459d07415bc06
DeathCode <organisetohack@gmail.com>
2017-12-28 01:32:26 +0530

    pickle file contained synchronized ticker data for every company

---
+++ second-stage2.py
@@ -0,0 +1,45 @@
+# S&P 500 companies
+import bs4 as bs
+import pickle
+import requests
+import datetime as dt
+import os
+import pandas_datareader as web
+import pandas as pd
+
+
+def save_sp500_tickers():
+    response = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(response.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+
+    with open('sp500tickers.pickle', 'wb') as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+    return tickers
+
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500tickers.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfc')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2017, 11, 30)
+
+    for ticker in tickers:
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have the file for {}'.format(ticker))
